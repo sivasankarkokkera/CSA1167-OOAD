a49163dae4f9b6fb814cd49c00e990a537813bdb
KOKKERA SIVASANKAR <112496249+sivasankarkokkera@users.noreply.github.com>
2023-05-15 14:33:50 +0530

    Add files via upload

---
+++ passenger.java
@@ -0,0 +1,73 @@
+
+
+/**
+ * Class new_class
+ */
+public class new_class {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public new_class () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+}
+
+
+
+/**
+ * Interface PASSENGER
+ */
+public interface PASSENGER {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void view_train_details();
+
+
+  /**
+   */
+  public void request_form();
+
+
+  /**
+   */
+  public void submit_form();
+
+
+}
